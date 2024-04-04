5515e5cd18050c41337e4a1dfe56a3479cb483c4
ad1tyad3sa1 <aditya7desai@gmail.com>
2024-04-04 06:48:31 -0700

    baking api finally works

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ from api.house_price import house_price_api
 # database migrations
 from model.users import initUsers
 from model.players import initPlayers
+from model.bakings import initBakings
 
 # setup APIs from first file
 from api.covid import covid_api
@@ -26,21 +27,26 @@ from model.bakeries import initbakery
 from api.titanic import titanic_api
 from api.food import food_api
 from api.bakery import bakery_api
+from api.baking import baking_api
 
 # Initialize the SQLAlchemy object to work with the Flask app instance
 db.init_app(app)
+
+# setup App pages
+from projects.projects import app_projects # Blueprint directory import projects definition
 
 # register URIs from both files
 app.register_blueprint(user_api)
 app.register_blueprint(player_api)
 app.register_blueprint(covid_api)
 app.register_blueprint(joke_api)
-app.register_blueprint(app_projects)
 app.register_blueprint(titanic_api)
 app.register_blueprint(food_api)
 app.register_blueprint(stocks_api)
 app.register_blueprint(bakery_api)
 app.register_blueprint(house_price_api)
+app.register_blueprint(baking_api)
+app.register_blueprint(app_projects)
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -70,7 +76,7 @@ def save_settings():
 @app.before_request
 def before_request():
     allowed_origin = request.headers.get('Origin')
-    if allowed_origin in ['http://localhost:4100', 'http://127.0.0.1:4100', 'https://davidl0914.github.io', 'https://nighthawkcoders.github.io']:
+    if allowed_origin in ['http://localhost:4100', 'http://127.0.0.1:4100', 'https://tuckergol.github.io/frontgang/']:
         cors._origins = allowed_origin
 
 # Create an AppGroup for custom commands
@@ -81,11 +87,16 @@ custom_cli = AppGroup('custom', help='Custom commands')
 def generate_data():
     initUsers()
     initPlayers()
+    initBakings()
     initfood()
     initbakery()
 
 # Register the custom command group with the Flask application
 app.cli.add_command(custom_cli)
+# @app.before_first_request
+def activate_job():
+    initUsers()
+    initBakings()
 
 # this runs the application on the development server
 if __name__ == "__main__":
